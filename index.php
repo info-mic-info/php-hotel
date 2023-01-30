0186d911cff96f205c06b5c340b1e73597e7377f
info-mic-info <jobsocial94@gmail.com>
2023-01-30 18:30:52 +0100

    complete

--- index.php
+++ index.php
@@ -50,19 +50,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <div class="container-fluid border text-bg-primary p-3 text-center">
+        <h1>TABELLA PHP CON CICLO FOREACH </h1>
+    </div>
+    <div class="container my-5">
+        <table class='table'>
+            <thead>
+                <?php foreach ($hotels[0] as $key => $value) { ?>
+                    <th class='border text-bg-primary p-3'>
+                        <?php echo strtoupper($key); ?>
+                    </th>
+                <?php
+                }
+                ?>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $item) {  ?>
+                    <tr>
+                        <?php foreach ($item as $key => $value) { ?>
+                            <td>
+                                <?php
+                                if ($key == 'parking') {
+                                    if ($value == true) {
+                                        $value = 'sì';
+                                    } else {
+                                        $value = 'no';
+                                    }
+                                }
+
+                                echo $value;
 
 
- <?php foreach 
-  ($hotels as $hotel){
-        foreach ($hotel as $elem){
-            echo $elem;
-        };
-    };
-    ?>  
+                                ?>
+                            </td>
+                        <?php } ?>
+                    </tr>
 
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
